f9f05dd3fc06351f595ac37e2cf3eccf74fe8e7c
abhishekkaddipudi123 <abhishekkaddipudi007@gmail.com>
2024-02-16 14:29:50 +0530

    messae

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, send_file, request
 from PIL import Image, ImageDraw
 
 from datetime import datetime
+import io
 
 app = Flask(__name__)
 
@@ -14,13 +15,12 @@ def age_image():
 
     # Create a blank image with white background
     width, height = 21, 21
-    background_color = (255, 255, 255)
-    img = Image.new('RGB', (width, height), background_color)
+    img = Image.new('RGBA', (width, height), (0, 0, 0, 0))
     draw = ImageDraw.Draw(img)
 
     # Define font and text color
    
-    text_color = (0, 0, 0)
+    text_color = (0, 0, 0,255)
 
     # Calculate text position
     text = f"{age}"
@@ -31,11 +31,12 @@ def age_image():
     draw.text(position, text, fill=text_color, )
 
     # Save image to a temporary file
-    img_path = 'temp_age_image.png'
-    img.save(img_path)
+    img_buffer = io.BytesIO()
+    img.save(img_buffer, format='PNG')
+    img_buffer.seek(0)
 
     # Return the image file as a response
-    return send_file(img_path, mimetype='image/png')
+    return send_file(img_buffer, mimetype='image/png')
 
 def get_age(year):
     # Extract the "birthyear" parameter from the URL query string
